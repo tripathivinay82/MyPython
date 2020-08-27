7bf1bea184ebb63d13ad53932f660a3b7d53108f
tripathivinay82 <60552624+tripathivinay82@users.noreply.github.com>
2020-08-26 20:49:46 -0400

    new files
    adding new files

---
+++ HR-Set-Add.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  8 21:00:46 2020
+
+@author: vinayt
+"""
+
+readme="https://www.hackerrank.com/challenges/py-set-add/problem"
+
+import sys
+
+def set_add(string):
+    
+    #string.remove(string[0])
+    #string=list(set(string))
+    #print(len(string))
+    
+    
+
+if __name__ == "__main__":
+    
+    l1=[]
+    for line in sys.stdin:
+        print(line.rstrip())
+    #print(l1)
+    #string=string.split(' ')
+    set_add(l1)
